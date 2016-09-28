0d47e712f224ca721adc768bfa2e9aace5bd5d61
wanghuiict <wanghuiict@gmail.com>
2016-09-29 07:18:30 +0800

    british flag: draw lines
    british flag: draw helper lines

--- british.py
+++ british.py
@@ -23,13 +23,26 @@ def topleft(t, w=W, h=H):
 
 def drawline(t, x0, y0, x1, y1):
    topleft(t)
-   t.fd(x0)
+   t.fd(x0*UNIT)
    t.rt(90)
-   t.fd(y0)
+   t.fd(y0*UNIT)
    t.lt(tan2angle((x1-x0)/(y1-y0)))
    t.pendown()
    t.fd(sqrt(pow(x1-x0, 2) + pow(y1-y0, 2)) * UNIT)
    t.penup()
+
+def drawrect(t, x0, y0, x1, y1):
+    topleft(t)
+    t.fd(x0 * UNIT)
+    t.rt(90)
+    t.fd(y0 * UNIT)
+    t.pendown()
+    for _ in range(2):
+        t.fd((y1 - y0) * UNIT)
+        t.lt(90)
+        t.fd((x1 - x0) * UNIT)
+        t.lt(90)
+    t.penup()
 
 ''' w  * h '''
 def draw_flag(t, color='red',w=30,h=20):
@@ -65,5 +78,35 @@ tess.speed(0)
 
 tess.color('grey')
 drawline(tess, 0, 0, 60, 30)
+drawline(tess, 60, 0, 0, 30)
+
+drawline(tess, 1/sin(atan(0.5)), 0, 60, 30 - 1/sin(atan(2.0)))
+drawline(tess, 2/sin(atan(0.5)), 0, 60, 30 - 2/sin(atan(2.0)))
+drawline(tess, 3/sin(atan(0.5)), 0, 60, 30 - 3/sin(atan(2.0)))
+
+drawline(tess, 0, 1/sin(atan(2.0)), 60 - 1/sin(atan(0.5)), 30)
+drawline(tess, 0, 2/sin(atan(2.0)), 60 - 2/sin(atan(0.5)), 30)
+drawline(tess, 0, 3/sin(atan(2.0)), 60 - 3/sin(atan(0.5)), 30)
+
+drawline(tess, 60 - 1/sin(atan(0.5)), 0, 0, 30 - 1/sin(atan(2.0)))
+drawline(tess, 60 - 2/sin(atan(0.5)), 0, 0, 30 - 2/sin(atan(2.0)))
+drawline(tess, 60 - 3/sin(atan(0.5)), 0, 0, 30 - 3/sin(atan(2.0)))
+
+drawline(tess, 60, 1/sin(atan(2.0)), 1/sin(atan(0.5)), 30)
+drawline(tess, 60, 2/sin(atan(2.0)), 2/sin(atan(0.5)), 30)
+drawline(tess, 60, 3/sin(atan(2.0)), 3/sin(atan(0.5)), 30)
+
+drawrect(tess,0, 0, 25, 10)
+drawrect(tess,0, 0, 27, 12)
+
+drawrect(tess, 35, 0, 60, 10)
+drawrect(tess, 33, 0, 60, 12)
+
+drawrect(tess,0, 18, 27, 30)
+drawrect(tess,0, 20, 25, 30)
+
+drawrect(tess,33, 18, 60, 30)
+drawrect(tess,35, 20, 60, 30)
+
 wn.exitonclick()
 
