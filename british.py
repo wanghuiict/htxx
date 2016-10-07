192cc54cab56e476c26ec4aba2955d0dec4a8d1a
wanghuiict <wanghuiict@gmail.com>
2016-10-07 21:09:01 +0800

    british flag: completed
    british flag  completed

--- british.py
+++ british.py
@@ -9,6 +9,8 @@ from math import *
 UNIT=12
 W=60*UNIT
 H=30*UNIT
+x=1/sin(atan(0.5))
+y=1/sin(atan(2.0))
 
 def tan2angle(x):
    return 180 * atan(x) / pi
@@ -102,90 +104,103 @@ def draw_flag(t, color='red',w=30,h=20):
     t.end_fill()
     t.penup()
 
+
+def draw_helper_lines():
+    drawline(tess, 0, 0, 60, 30)
+    drawline(tess, 60, 0, 0, 30)
+    
+    # top-right
+    drawline(tess, x, 0, 60, 30 - y)
+    drawline(tess, 2*x, 0, 60, 30 - 2*y)
+    drawline(tess, 3*x, 0, 60, 30 - 3*y)
+    
+    # bottom-left
+    drawline(tess, 0, y, 60 - x, 30)
+    drawline(tess, 0, 2*y, 60 - 2*x, 30)
+    drawline(tess, 0, 3*y, 60 - 3*x, 30)
+    
+    # top-left
+    drawline(tess, 60 - x, 0, 0, 30 - y)
+    drawline(tess, 60 - 2*x, 0, 0, 30 - 2*y)
+    drawline(tess, 60 - 3*x, 0, 0, 30 - 3*y)
+    
+    # bottom-right
+    drawline(tess, 60, y, x, 30)
+    drawline(tess, 60, 2*y, 2*x, 30)
+    drawline(tess, 60, 3*y, 3*x, 30)
+    
+    # top-left
+    drawrect(tess,0, 0, 25, 10)
+    drawrect(tess,0, 0, 27, 12)
+    
+    # top-right
+    drawrect(tess, 35, 0, 60, 10)
+    drawrect(tess, 33, 0, 60, 12)
+    
+    # bottom-left
+    drawrect(tess,0, 18, 27, 30)
+    drawrect(tess,0, 20, 25, 30)
+    
+    # bottom-right
+    drawrect(tess,33, 18, 60, 30)
+    drawrect(tess,35, 20, 60, 30)
+
+def draw_shapes():
+    tess.color('#CC0000')
+    bigcross=[(27,0),(27,12),(0,12),(0,18),(27,18),(27,30),(33,30),
+                    (33,18),(60,18),(60,12),(33,12),(33,0),(27,0)]
+    drawlines(tess, bigcross, True)
+   
+    stripes=[]
+    stripes.append([(0,0),(0, 2*y),(2 * (10 - 2*y), 10),(20,10),(0,0)])
+    stripes.append([(60,0),(60 - 2*x, 0),(60-(20 + 2*x), 10),(40,10),(60,0)])
+    stripes.append([(0,30),(2*x, 30),(20 + 2*x, 20),(20,20),(0,30)])
+    stripes.append([(60,30),(60, 30 - 2*y),(60 -(20 - 2*x), 20),(40,20),(60,30)])
+    for stripe in stripes:
+        drawlines(tess, stripe, True)
+    
+    tess.color('#003399')
+    tri=[]
+    tri.append([(0, 3*y), (0, 10), (2 * (10 - 3*y), 10), (0, 3*y)])
+    tri.append([(3*x, 0), (25, 0), (25, (25 - 3*x)/2), (3*x, 0)])
+    tri.append([(35,0), (60 - 3*x, 0), (35, (25 - 3*x)/2), (35,0)])
+    tri.append([(60, 3*y), (60, 10), (60 - 2 * (10 - 3*y), 10), (60, 3*y)])
+    tri.append([(0, 30 - 3*y), (0, 20), (2 * (10 - 3*y), 20), (0, 30 - 3*y)])
+    tri.append([(3*x, 30), (25, 30), (25, 30 - (25 - 3*x)/2), (3*x, 30)])
+    tri.append([(35,30), (60 - 3*x, 30), (35, 30 - (25 - 3*x)/2), (35,30)])
+    tri.append([(60, 30 - 3*y), (60, 20), (60 - 2 * (10 - 3*y), 20), (60, 30 - 3*y)])
+    for triangle in tri:
+        drawlines(tess, triangle, True)
+
 wn = turtle.Screen()
 wn.bgcolor("lightgreen")
 wn.title("British")
 wn.screensize(W, H)
 tess = turtle.Turtle()
 tess.hideturtle()
-tess.pensize(1)
 tess.penup()
 
+# delay some
+'''
+tess.speed(1)
+for _ in range(10):
+  tess.home()
+  tess.fd(200)
+'''
+
 tess.speed(5)
+tess.pensize(1)
 draw_flag(tess, color='#fff',w=W,h=H)
 
+tess.pensize(1)
 tess.color('grey')
-#drawline(tess, 10, 20, 30, 40)
-#drawline(tess, 20, 10, 30, 40)
-#drawline(tess, 20, 10, 5, 5)
-#drawlines(tess, [(10,10),(10,20),(20,20),(20,10),(10,10)], True)
-
-tess.speed(0)
-drawline(tess, 0, 0, 60, 30)
-drawline(tess, 60, 0, 0, 30)
-
-# top-right
-drawline(tess, 1/sin(atan(0.5)), 0, 60, 30 - 1/sin(atan(2.0)))
-drawline(tess, 2/sin(atan(0.5)), 0, 60, 30 - 2/sin(atan(2.0)))
-drawline(tess, 3/sin(atan(0.5)), 0, 60, 30 - 3/sin(atan(2.0)))
-
-# bottom-left
-drawline(tess, 0, 1/sin(atan(2.0)), 60 - 1/sin(atan(0.5)), 30)
-drawline(tess, 0, 2/sin(atan(2.0)), 60 - 2/sin(atan(0.5)), 30)
-drawline(tess, 0, 3/sin(atan(2.0)), 60 - 3/sin(atan(0.5)), 30)
-
-# top-left
-drawline(tess, 60 - 1/sin(atan(0.5)), 0, 0, 30 - 1/sin(atan(2.0)))
-drawline(tess, 60 - 2/sin(atan(0.5)), 0, 0, 30 - 2/sin(atan(2.0)))
-drawline(tess, 60 - 3/sin(atan(0.5)), 0, 0, 30 - 3/sin(atan(2.0)))
-
-# bottom-right
-drawline(tess, 60, 1/sin(atan(2.0)), 1/sin(atan(0.5)), 30)
-drawline(tess, 60, 2/sin(atan(2.0)), 2/sin(atan(0.5)), 30)
-drawline(tess, 60, 3/sin(atan(2.0)), 3/sin(atan(0.5)), 30)
-
-# top-left
-drawrect(tess,0, 0, 25, 10)
-drawrect(tess,0, 0, 27, 12)
-
-# top-right
-drawrect(tess, 35, 0, 60, 10)
-drawrect(tess, 33, 0, 60, 12)
-
-# bottom-left
-drawrect(tess,0, 18, 27, 30)
-drawrect(tess,0, 20, 25, 30)
-
-# bottom-right
-drawrect(tess,33, 18, 60, 30)
-drawrect(tess,35, 20, 60, 30)
-
-tess.color('#CC0000')
 tess.speed(5)
-bigcross=[(27,0),(27,12),(0,12),(0,18),(27,18),(27,30),(33,30),
-                (33,18),(60,18),(60,12),(33,12),(33,0),(27,0)]
-drawlines(tess, bigcross, True)
+draw_helper_lines()
+draw_shapes()
 
-top_left_band=[(0,0),(0, 2/sin(atan(2.0))),(2 * (10 - 2/sin(atan(2.0))), 10),(20,10),(0,0)]
-drawlines(tess, top_left_band, True)
-
-top_right_band=[(60,0),(60 - 2/sin(atan(0.5)), 0),(60-(20 + 2/sin(atan(0.5))), 10),(40,10),(60,0)]
-drawlines(tess, top_right_band, True)
-
-top_bottom_band=[(0,30),(2/sin(atan(0.5)), 30),(20 + 2/sin(atan(0.5)), 20),(20,20),(0,30)]
-drawlines(tess, top_bottom_band, True)
-
-bottom_right_band=[(60,30),(60, 30 - 2/sin(atan(2.0))),(60 -(20 - 2/sin(atan(0.5))), 20),(40,20),(60,30)]
-drawlines(tess, bottom_right_band, True)
-
-tess.color('#003399')
-tri=[(0, 3/sin(atan(2.0))), (0, 10), (2 * (10 - 3/sin(atan(2.0))), 10), (0, 3/sin(atan(2.0)))]
-drawlines(tess, tri, True)
-tri=[(3/sin(atan(0.5)), 0), (25, 0), (25, (25 - 3/sin(atan(0.5)))/2), (3/sin(atan(0.5)), 0)]
-drawlines(tess, tri, True)
-
-
-
+tess.color('white')
+tess.speed(0)
+draw_helper_lines()
+draw_shapes()
 
 wn.exitonclick()
-
