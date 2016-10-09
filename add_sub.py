8efd62d2db478bd8bacd0deb394e32ec59ebfeca
wanghuiict <wanghuiict@gmail.com>
2016-10-09 20:08:03 +0800

    add_sub support params
    total, minnum, pattern

--- add_sub.py
+++ add_sub.py
@@ -1,24 +1,51 @@
+import sys
 import random
 
-def _get2():
-    return(random.randint(1,99), random.randint(1,99))
+maxnum=100
 
-def print_add_carry(count=1):
+def _get2():
+    return(random.randint(1, maxnum - 1), random.randint(1, maxnum - 1))
+
+def print_add_carry(count=1, minum=1):
+    if minnum * 2 >= maxnum:
+        raise Exception('min number %s is too large. maxnum is %s' % (minnum, maxnum))
     while count > 0:
         a,b = _get2()
-        if a + b > 100 or a % 10 + b % 10  < 10:
+        if a < minum or b < minum:
+            continue
+        if a + b > maxnum or a % 10 + b % 10  < 10:
             continue
         print("%2d + %2d =" % (a,b))
         count -= 1
 
-def print_sub_abdication(count=1):
+def print_sub_abdication(count=1, minum=1):
     while count > 0:
         a,b = _get2()
+        if a < minum or b < minum:
+            continue
         if a - b <= 0 or a % 10 - b % 10  >= 0:
             continue
         print("%2d - %2d =" % (a,b))
         count -= 1
 
 if __name__ == '__main__':
-    print_add_carry(3)
-    print_sub_abdication(3)
+    total = 3
+    minnum = 10
+    pattern = 'mix' # add sub mix 
+    argn = len(sys.argv)
+    if argn > 1:
+        total = int(sys.argv[1])
+    if argn > 2:
+        minnum = int(sys.argv[2])
+    if pattern == 'mix':
+        add = total / 2
+        sub = total - add
+    elif pattern == 'add':
+        add = total
+        sub = 0
+    else:
+        add = 0
+        sub = total
+    print_add_carry(add, minnum)
+    print_sub_abdication(sub, minnum)
+
