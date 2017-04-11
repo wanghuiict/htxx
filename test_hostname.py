10a95422c306160b47ea7b2ffb320cdcc051cd59
wanghuiict <wanghuiict@gmail.com>
2017-04-11 10:00:20 +0800

    refine: test valid hostname
    测试是否是一个合法的主机名。

--- test_hostname.py
+++ test_hostname.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python
-#encoding:utf-8
+# encoding:utf-8
 import sys
 
 b = u'汉字123'
 
-print 'unicode', b, 'len',len(b)
+print 'unicode', b, 'len', len(b)
 
-li=[s for s in b]
+li = [s for s in b]
 findit = False
 for x in li:
-    if x.isalnum() == True:
-        if  x.isdigit() == True:
+    if x.isalnum():
+        if x.isdigit():
             findit = True
         break
-if findit == True:
-   print('除去汉字之后，字符串以数字开头')
+if findit:
+    print('除去汉字之后，字符串以数字开头')
 else:
-   print('除去汉字之后，字符串不以数字开头')
+    print('除去汉字之后，字符串不以数字开头')
 print('以上测试有误。')
 
 b = u'汉字_123'
 c = b.encode('utf-8')
 print 'utf-8', c, 'len', len(c)
 print('utf-8 :')
-li=[s for s in c]
+li = [s for s in c]
 findit = False
 for x in li:
     print ord(x)
     if ord(x) < 128:
         print(x)
-        if  x.isdigit() == True:
+        if x.isdigit():
             findit = True
         break
-if findit == True:
-   print('除去汉字之后，字符串以数字开头')
+if findit:
+    print('除去汉字之后，字符串以数字开头')
 else:
-   print('除去汉字之后，字符串不以数字开头')
+    print('除去汉字之后，字符串不以数字开头')
 print('以上测试正确。')
 print('结论：使用utf-8编码的str来进行判断是可行方法。')
 
-def ishostname(utf8str):
-    c = utf8str
-    li=[s for s in c]
 
-    if li[0].isalpha() == False:
-       return False
+def ishostname(utf8str):
+    hostn = utf8str
+    li = [s for s in hostn]
+
+    if not li[0].isalpha():
+        return False
 
     ret = True
     for x in li[1:]:
-        if x.isalnum() == True or x == '-':
+        if x.isalnum() or x == '-':
             continue
         ret = False
         break
@@ -56,7 +57,7 @@ def ishostname(utf8str):
         ret = False
     return ret
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print("请使用UTF-8格式字符串")
     b = 'a1-23-'
     if len(sys.argv) == 2:
